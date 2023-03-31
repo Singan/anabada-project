c36720c30d648bb318ccf52431a007af448cec87
석성희 <sunghee119z@naver.com>
2023-03-31 22:23:52 +0900

    깃 테스트

--- anabada/src/main/java/com/anabada/controller/MemberController.java
+++ anabada/src/main/java/com/anabada/controller/MemberController.java
@@ -2,13 +2,15 @@ package com.anabada.controller;
 
 import com.anabada.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
-
-
-
+    @GetMapping
+    public String aa(){
+        return "aa";
+    }
 }
