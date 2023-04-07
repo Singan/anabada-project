3451e24ccf17e1ad9ca39ae9717f23748a9ed536
Hwi <124661622+Imhwitae@users.noreply.github.com>
2023-04-08 02:20:57 +0900

    Update ProductRepository.java

--- anabada/src/main/java/com/anabada/repository/ProductRepository.java
+++ anabada/src/main/java/com/anabada/repository/ProductRepository.java
@@ -4,7 +4,9 @@ import com.anabada.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
-
 }
