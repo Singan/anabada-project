4260f89bf4def8445e3c320236c61a7f3bf04f41
Hwi <124661622+Imhwitae@users.noreply.github.com>
2023-04-12 14:35:48 +0900

    bid dto 제작중

---
+++ anabada/src/main/java/com/anabada/controller/BidController.java
@@ -0,0 +1,15 @@
+package com.anabada.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/bid")
+public class BidController {
+
+    @PostMapping
+
+}
