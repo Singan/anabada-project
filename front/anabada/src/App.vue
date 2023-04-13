d0785f4591d95099a637777203f5d68b84fad4f7
simsimhang <seajin97@g.shingu.ac.kr>
2023-04-13 11:32:25 +0900

    d
    d

---
+++ front/anabada/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <MainPage></MainPage>
+    
+  </div>
+</template>
+
+<script>
+import MainPage from './components/MainPage.vue'
+
+export default {
+  components: {
+    'MainPage':MainPage
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ front/anabada/src/components/MainPage.vue
@@ -0,0 +1,600 @@
+<template>
+<div class="--">
+    <div class="--2">
+      <div class="--3">경매</div>
+
+      <div class="----">아나바다</div>
+
+      <div class="--4">채팅</div>
+
+      <div class="---"></div>
+
+      <div class="--5">
+        <div class="----2"></div>
+
+        <div class="----3">
+          <div class="_18px">검색하기</div>
+        </div>
+      </div>
+
+      <div class="---2">
+        <div class="----4"></div>
+
+        <div class="----5">
+          <div class="_18px2">로그인</div>
+        </div>
+      </div>
+    </div>
+
+    <div class="---1"></div>
+
+    <div class="---22"></div>
+
+    <div class="---3"></div>
+
+    <div class="--3---">
+      <div class="--6">현재 경매 되고 있는 상품을 둘러보세요</div>
+    </div>
+
+    <div class="--2---">
+      <div class="--7">내가 사는 지역에서 펼쳐지는 경매장</div>
+    </div>
+
+    <div class="---4">
+      <div class="--8">원하는 물품,<br />원하는 가격으로</div>
+    </div>
+
+    <div class="--9">
+      <div class="----6">
+        중고 거래, 이제 경매에 직접 참여해서<br />원하는 가격으로 구매해보세요
+      </div>
+    </div>
+
+    <div class="-------">
+      <div class="rectangle-13"></div>
+
+      <div class="rectangle-14"></div>
+
+      <div class="rectangle-12"></div>
+    </div>
+
+    <div class="---------"></div>
+
+    <div class="---------2"></div>
+
+    <div class="---------3"></div>
+
+    <div class="---------4"></div>
+
+    <div class="-------2"></div>
+
+    <div class="-------1"></div>
+
+    <div class="-----14---">
+      <div class="----7">아이폰 14 프로</div>
+    </div>
+
+    <div class="-----14---2">
+      <div class="----8">경기도 성남시</div>
+    </div>
+
+    <div class="--10">
+      <div class="----9">1,200,000 원</div>
+    </div>
+
+    <div class="-----14---3">
+      <div class="----7">위닉스 제습기</div>
+    </div>
+
+    <div class="-----14---4">
+      <div class="----8">경기도 성남시</div>
+    </div>
+
+    <div class="--11">
+      <div class="----9">32,000 원</div>
+    </div>
+
+    <div class="-----14---5">
+      <div class="----10">LG 코드제로</div>
+    </div>
+
+    <div class="-----14---6">
+      <div class="----8">경기도 성남시</div>
+    </div>
+
+    <div class="--12">
+      <div class="----11">600,000 원</div>
+    </div>
+
+    <div class="-----14---7">
+      <div class="----8">갤럭시 s22 울트라 256gb</div>
+    </div>
+
+    <div class="-----14---8">
+      <div class="----8">경기도 성남시</div>
+    </div>
+
+    <div class="--13">
+      <div class="----9">900,000 원</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+.--,
+.-- * {
+  box-sizing: border-box;
+}
+.-- {
+  background: #ffffff;
+  width: 1920px;
+  height: 1900px;
+  position: relative;
+  overflow: hidden;
+}
+.--2 {
+  width: 1130px;
+  height: 40px;
+  position: static;
+}
+.--3 {
+  color: #0075ff;
+  text-align: center;
+  font: 700 18px "Roboto", sans-serif;
+  position: absolute;
+  left: 575px;
+  top: 40px;
+  width: 40px;
+  height: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.---- {
+  color: #0075ff;
+  text-align: center;
+  font: 700 24px "Roboto", sans-serif;
+  position: absolute;
+  left: 395px;
+  top: 35px;
+  width: 110px;
+  height: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.--4 {
+  color: #000000;
+  text-align: center;
+  font: 700 18px "Roboto", sans-serif;
+  position: absolute;
+  left: 655px;
+  top: 40px;
+  width: 40px;
+  height: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.--- {
+  background: #d9ecff;
+  border-radius: 5px;
+  width: 450px;
+  height: 40px;
+  position: absolute;
+  left: 785px;
+  top: 30px;
+}
+.--5 {
+  width: 130px;
+  height: 40px;
+  position: static;
+}
+.----2 {
+  background: #ffffff;
+  border-radius: 5px;
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px;
+  width: 130px;
+  height: 40px;
+  position: absolute;
+  left: 1265px;
+  top: 30px;
+}
+.----3 {
+  width: 80px;
+  height: 30px;
+  position: absolute;
+  left: 1290px;
+  top: 35px;
+}
+._18px {
+  color: #000000;
+  text-align: center;
+  font: 700 18px "Roboto", sans-serif;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 80px;
+  height: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.---2 {
+  width: 80px;
+  height: 40px;
+  position: static;
+}
+.----4 {
+  background: #0075ff;
+  border-radius: 5px;
+  width: 80px;
+  height: 40px;
+  position: absolute;
+  left: 1445px;
+  top: 30px;
+}
+.----5 {
+  width: 80px;
+  height: 30px;
+  position: absolute;
+  left: 1445px;
+  top: 35px;
+}
+._18px2 {
+  color: #ffffff;
+  text-align: center;
+  font: 700 18px "Roboto", sans-serif;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 80px;
+  height: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.---1 {
+  background: #f1f8ff;
+  width: 1920px;
+  height: 600px;
+  position: absolute;
+  left: 0px;
+  top: 100px;
+}
+.---22 {
+  background: #f3f4ff;
+  width: 1920px;
+  height: 600px;
+  position: absolute;
+  left: 0px;
+  top: 700px;
+}
+.---3 {
+  background: #f6efff;
+  width: 1920px;
+  height: 600px;
+  position: absolute;
+  left: 0px;
+  top: 1300px;
+}
+.--3--- {
+  width: 693px;
+  height: 60px;
+  position: absolute;
+  left: 613px;
+  top: 1380px;
+}
+.--6 {
+  color: #000000;
+  text-align: center;
+  font: 700 33px "Roboto", sans-serif;
+  position: absolute;
+  left: 83px;
+  top: 11px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.--2--- {
+  width: 666px;
+  height: 60px;
+  position: absolute;
+  left: 627px;
+  top: 780px;
+}
+.--7 {
+  color: #000000;
+  text-align: center;
+  font: 700 35px "Roboto", sans-serif;
+  position: absolute;
+  left: 90px;
+  top: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.---4 {
+  width: 307px;
+  height: 80px;
+  position: absolute;
+  left: 333px;
+  top: 290px;
+}
+.--8 {
+  color: #000000;
+  text-align: left;
+  font: 700 35px "Roboto", sans-serif;
+  position: absolute;
+  left: 0px;
+  top: -1px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+.--9 {
+  width: 334px;
+  height: 50px;
+  position: absolute;
+  left: 333px;
+  top: 404px;
+}
+.----6 {
+  color: #000000;
+  text-align: left;
+  font: 400 16px "Roboto", sans-serif;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 334px;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+.------- {
+  width: 773.33px;
+  height: 358px;
+  position: static;
+}
+.rectangle-13 {
+  background: #bdbdbd;
+  width: 400px;
+  height: 190px;
+  position: absolute;
+  left: 1000px;
+  top: 368px;
+  box-shadow: 5px 4px 4px 0px rgba(0, 0, 0, 0.25);
+}
+.rectangle-14 {
+  background: #bdbdbd;
+  width: 400px;
+  height: 190px;
+  position: absolute;
+  left: 1240px;
+  top: 200px;
+  box-shadow: 5px 4px 4px 0px rgba(0, 0, 0, 0.25);
+}
+.rectangle-12 {
+  background: #bdbdbd;
+  width: 400px;
+  height: 190px;
+  position: absolute;
+  left: 866.67px;
+  top: 256px;
+  box-shadow: 5px 4px 4px 0px rgba(0, 0, 0, 0.25);
+}
+.--------- {
+  background: #bdbdbd;
+  border-radius: 5px;
+  width: 240px;
+  height: 180px;
+  position: absolute;
+  left: 320px;
+  top: 1490px;
+}
+.---------2 {
+  background: #bdbdbd;
+  border-radius: 5px;
+  width: 240px;
+  height: 180px;
+  position: absolute;
+  left: 667px;
+  top: 1490px;
+}
+.---------3 {
+  background: #bdbdbd;
+  border-radius: 5px;
+  width: 240px;
+  height: 180px;
+  position: absolute;
+  left: 1013px;
+  top: 1490px;
+}
+.---------4 {
+  background: #bdbdbd;
+  border-radius: 5px;
+  width: 240px;
+  height: 180px;
+  position: absolute;
+  left: 1360px;
+  top: 1490px;
+}
+.-------2 {
+  background: #bdbdbd;
+  width: 600px;
+  height: 270px;
+  position: absolute;
+  left: 1000px;
+  top: 900px;
+  box-shadow: 5px 4px 4px 0px rgba(0, 0, 0, 0.25);
+}
+.-------1 {
+  background: #bdbdbd;
+  width: 600px;
+  height: 270px;
+  position: absolute;
+  left: 320px;
+  top: 900px;
+  box-shadow: 5px 4px 4px 0px rgba(0, 0, 0, 0.25);
+}
+.-----14--- {
+  width: 133px;
+  height: 20px;
+  position: absolute;
+  left: 320px;
+  top: 1680px;
+}
+.----7 {
+  color: #000000;
+  text-align: left;
+  font: 400 16px "Roboto", sans-serif;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 133px;
+  height: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+.-----14---2 {
+  width: 240px;
+  height: 20px;
+  position: absolute;
+  left: 320px;
+  top: 1730px;
+}
+.----8 {
+  color: #000000;
+  text-align: left;
+  font: 400 16px "Roboto", sans-serif;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 240px;
+  height: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+.--10 {
+  width: 133px;
+  height: 20px;
+  position: absolute;
+  left: 320px;
+  top: 1705px;
+}
+.----9 {
+  color: #000000;
+  text-align: left;
+  font: 700 16px "Roboto", sans-serif;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 133px;
+  height: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+.-----14---3 {
+  width: 133px;
+  height: 20px;
+  position: absolute;
+  left: 667px;
+  top: 1680px;
+}
+.-----14---4 {
+  width: 240px;
+  height: 20px;
+  position: absolute;
+  left: 667px;
+  top: 1730px;
+}
+.--11 {
+  width: 133px;
+  height: 20px;
+  position: absolute;
+  left: 667px;
+  top: 1705px;
+}
+.-----14---5 {
+  width: 134px;
+  height: 20px;
+  position: absolute;
+  left: 1013px;
+  top: 1680px;
+}
+.----10 {
+  color: #000000;
+  text-align: left;
+  font: 400 16px "Roboto", sans-serif;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 134px;
+  height: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+.-----14---6 {
+  width: 240px;
+  height: 20px;
+  position: absolute;
+  left: 1013px;
+  top: 1730px;
+}
+.--12 {
+  width: 134px;
+  height: 20px;
+  position: absolute;
+  left: 1013px;
+  top: 1705px;
+}
+.----11 {
+  color: #000000;
+  text-align: left;
+  font: 700 16px "Roboto", sans-serif;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 134px;
+  height: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+.-----14---7 {
+  width: 240px;
+  height: 20px;
+  position: absolute;
+  left: 1360px;
+  top: 1680px;
+}
+.-----14---8 {
+  width: 240px;
+  height: 20px;
+  position: absolute;
+  left: 1360px;
+  top: 1730px;
+}
+.--13 {
+  width: 133px;
+  height: 20px;
+  position: absolute;
+  left: 1360px;
+  top: 1705px;
+}
+
+</style>
