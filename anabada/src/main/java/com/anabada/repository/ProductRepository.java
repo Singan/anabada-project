ca43151c92d61cbae3152f076c04d9d2681fa1ae
Hwi <124661622+Imhwitae@users.noreply.github.com>
2023-04-15 00:09:44 +0900

    Orders삭제 + Category db에 값 넣을것

--- anabada/src/main/java/com/anabada/repository/ProductRepository.java
+++ anabada/src/main/java/com/anabada/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package com.anabada.repository;
 import com.anabada.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,5 +11,5 @@ import java.util.Optional;
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
     @Query("select p from Product p join fetch p.member where p.productNo=:id")
-    Optional<Product> findById(Long id);
+    Optional<Product> findById(@Param("id") Long id);
 }
