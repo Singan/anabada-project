dbd4fe2287369f751a2bad0f35b616b306b64b46
simsimhang <seajin97@g.shingu.ac.kr>
2023-04-16 00:20:21 +0900

    s
    s

---
+++ front/anabadaonline/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    
+    <router-view />
+
+  </div>
+</template>
+
+<script>
+
+import MainPage from './components/MainPage.vue'
+
+export default {
+  components: {
+    "MainPage": MainPage,
+    
+}
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ front/anabadaonline/src/router.js
@@ -0,0 +1,39 @@
+// 라우트 정보
+// 라우팅을 해주면서 #을 제거해주면서, path에 입력한 경로에 매핑된 컴포넌트를 보여준다.
+
+
+const routes = [
+    {
+        path: '/',
+        name: 'Main',
+        component: () => import('@/components/MainPage.vue'),
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: () => import('@/components/LoginPage.vue'),
+    },
+    {
+        path: '/mypage',
+        name: 'MyPage',
+        component: () => import('@/components/MyPage.vue'),
+    },
+    {
+        path: '/product',
+        name: 'ProductInsert',
+        component: () => import('@/components/ProductRegistrationPage.vue'),
+    },
+    {
+        path: '/auction',
+        name: 'Auction',
+        component: () => import('@/components/Auction.vue'),
+    },
+    {
+        path: '/chat',
+        name: 'Chat',
+        component: () => import('@/components/chat.vue'),
+    },
+]
+
+
+export default routes;
