e91927b74928cef87cbfd00e496ddcf1b14973ed
seung-min-0109 <127377279+seung-min-0109@users.noreply.github.com>
2023-04-16 20:14:35 +0900

    메인페이지 클래스이름 변경(완)

--- front/anabada/src/components/MainPage.vue
+++ front/anabada/src/components/MainPage.vue
@@ -13,45 +13,45 @@
       <div class="Search">
         <div class="SearchButton"></div>
         <div class="SearchText">
-          <div class="Text1">검색하기</div>
+          <div class="SearchText1">검색하기</div>
         </div>
       </div>
 
       <div class="Login">
         <div class="LoginBox"></div>
         <div class="LoginText">
-          <div class="Text2" @click="goLogin">
+          <div class="LoginText1" @click="goLogin">
             로그인
           </div>          
         </div>
       </div>
     </div>
 
-    <div class="---1"></div>
+    <div class="Footer1"></div>
 
-    <div class="---22"></div>
+    <div class="Footer2"></div>
 
-    <div class="---3"></div>
+    <div class="Footer3"></div>
 
-    <div class="--3---">
-      <div class="--6">현재 경매 되고 있는 상품을 둘러보세요</div>
+    <div class="TextBox1">
+      <div class="Text1">현재 경매 되고 있는 상품을 둘러보세요</div>
     </div>
 
-    <div class="--2---">
-      <div class="--7">내가 사는 지역에서 펼쳐지는 경매장</div>
+    <div class="TextBox2">
+      <div class="Text2">내가 사는 지역에서 펼쳐지는 경매장</div>
     </div>
 
-    <div class="---4">
-      <div class="--8">원하는 물품,<br />원하는 가격으로</div>
+    <div class="TextBox3">
+      <div class="Text3">원하는 물품,<br />원하는 가격으로</div>
     </div>
 
-    <div class="--9">
-      <div class="----6">
+    <div class="TextBox4">
+      <div class="Text4">
         중고 거래, 이제 경매에 직접 참여해서<br />원하는 가격으로 구매해보세요
       </div>
     </div>
 
-    <div class="-------">
+    <div class="ImgArea1">
       <div class="rectangle-13">
         <img src="@/assets/bike.jpg" style="width:100%; height:130%;"/>
       </div>
@@ -65,68 +65,68 @@
       </div>
     </div>
 
-    <div class="---------"></div>
+    <div class="ProductImg1"></div>
 
-    <div class="---------2"></div>
+    <div class="ProductImg2"></div>
 
-    <div class="---------3"></div>
+    <div class="ProductImg3"></div>
 
-    <div class="---------4"></div>
+    <div class="ProductImg4"></div>
 
-    <div class="-------2">
+    <div class="Illust2">
       <img src="@/assets/change2.jpg" style="width:100%; height:100%;"/>
     </div>
 
-    <div class="-------1">
+    <div class="Illust1">
       <img src="@/assets/change.png" style="width:100%; height:100%;"/>
     </div>
 
-    <div class="-----14---">
-      <div class="----7">아이폰 14 프로</div>
+    <div class="ProductN1">
+      <div class="ProductNT1">아이폰 14 프로</div>
     </div>
 
-    <div class="-----14---2">
-      <div class="----8">경기도 성남시</div>
+    <div class="Adress1">
+      <div class="AdressT1">경기도 성남시</div>
     </div>
 
-    <div class="--10">
-      <div class="----9">1,200,000 원</div>
+    <div class="Price1">
+      <div class="PriceT1">1,200,000 원</div>
     </div>
 
-    <div class="-----14---3">
-      <div class="----7">위닉스 제습기</div>
+    <div class="ProductN2">
+      <div class="ProductNT2">위닉스 제습기</div>
     </div>
 
-    <div class="-----14---4">
-      <div class="----8">경기도 성남시</div>
+    <div class="Adress2">
+      <div class="AdressT1">경기도 성남시</div>
     </div>
 
-    <div class="--11">
-      <div class="----9">32,000 원</div>
+    <div class="Price2">
+      <div class="PriceT1">32,000 원</div>
     </div>
 
-    <div class="-----14---5">
-      <div class="----10">LG 코드제로</div>
+    <div class="ProductN3">
+      <div class="ProductNT1">LG 코드제로</div>
     </div>
 
-    <div class="-----14---6">
-      <div class="----8">경기도 성남시</div>
+    <div class="Adress3">
+      <div class="AdressT1">경기도 성남시</div>
     </div>
 
-    <div class="--12">
-      <div class="----11">600,000 원</div>
+    <div class="Price3">
+      <div class="PriceT1">600,000 원</div>
     </div>
 
-    <div class="-----14---7">
-      <div class="----8">갤럭시 s22 울트라 256gb</div>
+    <div class="ProductN4">
+      <div class="ProductNT1">갤럭시 s22 울트라 256gb</div>
     </div>
 
-    <div class="-----14---8">
-      <div class="----8">경기도 성남시</div>
+    <div class="Adress4">
+      <div class="AdressT1">경기도 성남시</div>
     </div>
 
-    <div class="--13">
-      <div class="----9">900,000 원</div>
+    <div class="Price4">
+      <div class="PriceT1">900,000 원</div>
     </div>
   </div>
 </template>
@@ -153,18 +153,18 @@ export default {
 
 <style scoped>
 
-.header,
-.header * {
+.Header,
+.Header * {
   box-sizing: border-box;
 }
-.header {
+.Header {
   background: #ffffff;
   width: 1920px;
   height: 1900px;
   position: relative;
   overflow: hidden;
 }
-.headerButton {
+.HeaderButton {
   width: 1130px;
   height: 40px;
   position: static;
@@ -243,7 +243,7 @@ export default {
   left: 1290px;
   top: 35px;
 }
-.Text1 {
+.SearchText1 {
   color: #000000;
   text-align: center;
   font: 700 18px "Roboto", sans-serif;
@@ -277,7 +277,7 @@ export default {
   left: 1445px;
   top: 35px;
 }
-.Text2 {
+.LoginText1 {
   color: #ffffff;
   text-align: center;
   font: 700 18px "Roboto", sans-serif;
@@ -290,7 +290,7 @@ export default {
   align-items: center;
   justify-content: center;
 }
-.---1 {
+.Footer1 {
   background: #f1f8ff;
   width: 1920px;
   height: 600px;
@@ -298,7 +298,7 @@ export default {
   left: 0px;
   top: 100px;
 }
-.---22 {
+.Footer2 {
   background: #f3f4ff;
   width: 1920px;
   height: 600px;
@@ -306,7 +306,7 @@ export default {
   left: 0px;
   top: 700px;
 }
-.---3 {
+.Footer3 {
   background: #f6efff;
   width: 1920px;
   height: 600px;
@@ -314,14 +314,14 @@ export default {
   left: 0px;
   top: 1300px;
 }
-.--3--- {
+.TextBox1 {
   width: 693px;
   height: 60px;
   position: absolute;
   left: 613px;
   top: 1380px;
 }
-.--6 {
+.Text1 {
   color: #000000;
   text-align: center;
   font: 700 33px "Roboto", sans-serif;
@@ -332,14 +332,14 @@ export default {
   align-items: center;
   justify-content: center;
 }
-.--2--- {
+.TextBox2 {
   width: 666px;
   height: 60px;
   position: absolute;
   left: 627px;
   top: 780px;
 }
-.--7 {
+.Text2 {
   color: #000000;
   text-align: center;
   font: 700 35px "Roboto", sans-serif;
@@ -350,14 +350,14 @@ export default {
   align-items: center;
   justify-content: center;
 }
-.---4 {
+.TextBox3 {
   width: 307px;
   height: 80px;
   position: absolute;
   left: 333px;
   top: 290px;
 }
-.--8 {
+.Text3 {
   color: #000000;
   text-align: left;
   font: 700 35px "Roboto", sans-serif;
@@ -368,14 +368,14 @@ export default {
   align-items: center;
   justify-content: flex-start;
 }
-.--9 {
+.TextBox4 {
   width: 334px;
   height: 50px;
   position: absolute;
   left: 333px;
   top: 404px;
 }
-.----6 {
+.Text4 {
   color: #000000;
   text-align: left;
   font: 400 16px "Roboto", sans-serif;
@@ -388,7 +388,7 @@ export default {
   align-items: center;
   justify-content: flex-start;
 }
-.------- {
+.ImgArea1 {
   width: 773.33px;
   height: 358px;
   position: static;
@@ -420,7 +420,7 @@ export default {
   top: 256px;
   box-shadow: 5px 4px 4px 0px rgba(0, 0, 0, 0.25);
 }
-.--------- {
+.ProductImg1 {
   background: #bdbdbd;
   border-radius: 5px;
   width: 240px;
@@ -429,7 +429,7 @@ export default {
   left: 320px;
   top: 1490px;
 }
-.---------2 {
+.ProductImg2 {
   background: #bdbdbd;
   border-radius: 5px;
   width: 240px;
@@ -438,7 +438,7 @@ export default {
   left: 667px;
   top: 1490px;
 }
-.---------3 {
+.ProductImg3 {
   background: #bdbdbd;
   border-radius: 5px;
   width: 240px;
@@ -447,7 +447,7 @@ export default {
   left: 1013px;
   top: 1490px;
 }
-.---------4 {
+.ProductImg4 {
   background: #bdbdbd;
   border-radius: 5px;
   width: 240px;
@@ -456,7 +456,7 @@ export default {
   left: 1360px;
   top: 1490px;
 }
-.-------2 {
+.Illust2 {
   background: #bdbdbd;
   width: 600px;
   height: 270px;
@@ -465,7 +465,7 @@ export default {
   top: 900px;
   box-shadow: 5px 4px 4px 0px rgba(0, 0, 0, 0.25);
 }
-.-------1 {
+.Illust1 {
   background: #bdbdbd;
   width: 600px;
   height: 270px;
@@ -474,34 +474,34 @@ export default {
   top: 900px;
   box-shadow: 5px 4px 4px 0px rgba(0, 0, 0, 0.25);
 }
-.-----14--- {
+.ProductN1 {
   width: 133px;
   height: 20px;
   position: absolute;
   left: 320px;
   top: 1680px;
 }
-.----7 {
+.ProductNT1 {
   color: #000000;
   text-align: left;
   font: 400 16px "Roboto", sans-serif;
   position: absolute;
   left: 0px;
   top: 0px;
-  width: 133px;
+  width: 240px;
   height: 20px;
   display: flex;
   align-items: center;
   justify-content: flex-start;
 }
-.-----14---2 {
+.Adress1 {
   width: 240px;
   height: 20px;
   position: absolute;
   left: 320px;
   top: 1730px;
 }
-.----8 {
+.AdressT1 {
   color: #000000;
   text-align: left;
   font: 400 16px "Roboto", sans-serif;
@@ -514,14 +514,14 @@ export default {
   align-items: center;
   justify-content: flex-start;
 }
-.--10 {
+.Price1 {
   width: 133px;
   height: 20px;
   position: absolute;
   left: 320px;
   top: 1705px;
 }
-.----9 {
+.PriceT1 {
   color: #000000;
   text-align: left;
   font: 700 16px "Roboto", sans-serif;
@@ -534,28 +534,28 @@ export default {
   align-items: center;
   justify-content: flex-start;
 }
-.-----14---3 {
+.ProductNT2 {
   width: 133px;
   height: 20px;
   position: absolute;
   left: 667px;
   top: 1680px;
 }
-.-----14---4 {
+.Adress2 {
   width: 240px;
   height: 20px;
   position: absolute;
   left: 667px;
   top: 1730px;
 }
-.--11 {
+.Price2 {
   width: 133px;
   height: 20px;
   position: absolute;
   left: 667px;
   top: 1705px;
 }
-.-----14---5 {
+.ProductN3 {
   width: 134px;
   height: 20px;
   position: absolute;
@@ -575,14 +575,14 @@ export default {
   align-items: center;
   justify-content: flex-start;
 }
-.-----14---6 {
+.Adress3 {
   width: 240px;
   height: 20px;
   position: absolute;
   left: 1013px;
   top: 1730px;
 }
-.--12 {
+.Price3 {
   width: 134px;
   height: 20px;
   position: absolute;
@@ -602,21 +602,21 @@ export default {
   align-items: center;
   justify-content: flex-start;
 }
-.-----14---7 {
+.ProductN4 {
   width: 240px;
   height: 20px;
   position: absolute;
   left: 1360px;
   top: 1680px;
 }
-.-----14---8 {
+.Adress4 {
   width: 240px;
   height: 20px;
   position: absolute;
   left: 1360px;
   top: 1730px;
 }
-.--13 {
+.Price4 {
   width: 133px;
   height: 20px;
   position: absolute;
