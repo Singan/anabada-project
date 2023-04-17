76faa4fa7f21c0a5c82d24ca6695cf1c5ac68371
simsimhang <seajin97@g.shingu.ac.kr>
2023-04-17 22:45:38 +0900

    d
    d

--- front/anabadaonline/src/components/MemberJoin.vue
+++ front/anabadaonline/src/components/MemberJoin.vue
@@ -1,39 +1,42 @@
 <template>
 	<div class="container">
 		<form @submit.prevent="submitForm">
-			<div>
+			
+			<div >
 				<label for="id">id</label>
 				<input type="text" id="email" v-model="id" />
 			</div>
-			<div>
+			<div >
 				<label for="pw">pw</label>
 				<input type="password" id="pw" v-model="pw" />
 			</div>
-			<div>
+			<div > 
 				<label for="name">이름</label>
 				<input type="text" id="name" v-model="name" />
 			</div>
-			<div>
+			<div >
 				<label for="birth">생년월일</label>
 				<input type="date" id="birth" v-model="birth" />
 			</div>
-            <div>
+            <div >
 				<label for="addr">주소</label>
 				<input type="text" id="addr" v-model="addr" />
 			</div>
-			<div>
+			<div >
 				<label for="detailaddr">상세주소</label>
 				<input type="text" id="detailaddr" v-model="detailaddr" />
 			</div>
-            <div>
+            <div >
 				<label for="Wishaddr">거래희망지</label>
 				<input type="text" id="Wishaddr" v-model="Wishaddr" />
 			</div>
-            <div>
+            <div >
 				<label for="image">프로필 이미지</label>
 				<input id="image" type="file" @change="onFileChange">
 			</div>
+			<div>
 			<button type="submit">회원가입</button>
+			</div>
 		</form>
 	</div>
 </template>
@@ -65,4 +68,6 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
