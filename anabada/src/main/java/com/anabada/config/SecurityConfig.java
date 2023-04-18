0d33706da480980c317929262d3e913b199647b6
석성희 <sunghee119z@naver.com>
2023-04-18 23:07:40 +0900

    ㅇ

--- anabada/src/main/java/com/anabada/config/SecurityConfig.java
+++ anabada/src/main/java/com/anabada/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig   {
             "/swagger-ui",
             "/swagger-ui.html",
             "/api-docs",
-            "/v3/api-docs/"
+            "/v3/api-docs/**"
     };
     /**
      * 1. 정적 자원(Resource)에 대해서 인증된 사용자가  정적 자원의 접근에 대해 ‘인가’에 대한 설정을 담당하는 메서드이다.
