4a32fad88aea86b9cdc3dbf75baba360f4bb47cd
석성희 <sunghee119z@naver.com>
2023-04-19 22:37:34 +0900

    d

--- front/anabadaonline/src/main.js
+++ front/anabadaonline/src/main.js
@@ -2,7 +2,7 @@ import App from './App.vue'
 import routes from './router'
 import { createRouter, createWebHistory } from "vue-router";
 import { createApp } from 'vue'
-import axios from '@/axios';
+
 
 let router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
@@ -17,4 +17,3 @@ const app = createApp(App).use(router);
 app.mount("#app");
 
 
-app.config.globalProperties.$axios = axios; 
