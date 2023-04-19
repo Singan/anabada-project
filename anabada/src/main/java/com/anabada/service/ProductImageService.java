72f3c9ad9e12be9eaf79d40cbb73e09fb8d76297
석성희 <sunghee119z@naver.com>
2023-04-19 23:21:30 +0900

    d
    d

--- anabada/src/main/java/com/anabada/service/ProductImageService.java
+++ anabada/src/main/java/com/anabada/service/ProductImageService.java
@@ -30,10 +30,11 @@ public class ProductImageService {
         Product product = Product.builder().productNo(no).build();
         List<ProductImage> productImageList = imagePathList.stream().map
                 (s -> ProductImage.
-                        builder()
-                        .product(product)
-                        .build()
-                ).collect(Collectors.toList());
+                        builder().
+                        product(product).
+                        imageAddr(s).
+                        build()
+        ).collect(Collectors.toList());
         productRepository.saveAll(productImageList);
     }
 }
