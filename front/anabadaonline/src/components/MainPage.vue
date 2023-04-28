1973a42fc2b9ba4b000829d522de5ff4af8edfac
석성희 <sunghee119z@naver.com>
2023-04-28 23:44:48 +0900

    d

--- front/anabadaonline/src/components/MainPage.vue
+++ front/anabadaonline/src/components/MainPage.vue
@@ -1,460 +1,287 @@
 <template>
-	<div class="content1 flexContainer">
-		<div class="Text3">
-			원하는 물품,<br />원하는 가격으로<br /><br />
-			<div class="Text4">
-				중고 거래, 이제 경매에 직접 참여해서<br />원하는 가격으로
-				구매해보세요
-			</div>
-		</div>
-		<div class="topImages">
-			<img src="@/assets/mainImage.png" />
-			<!-- <img class="rectangle-14" src="@/assets/samsung.jpg" />
+    <div class="content1 flexContainer">
+        <div class="Text3">
+            원하는 물품,<br />원하는 가격으로<br /><br />
+            <div class="Text4">
+                중고 거래, 이제 경매에 직접 참여해서<br />원하는 가격으로
+                구매해보세요
+            </div>
+        </div>
+        <div class="topImages">
+            <img src="@/assets/mainImage.png" />
+            <!-- <img class="rectangle-14" src="@/assets/samsung.jpg" />
 			<img class="rectangle-12" src="@/assets/iphone14.jpg" /> -->
-		</div>
-	</div>
+        </div>
+    </div>
 
-	<div class="Text2 content2TextFlex content2Title">
-		내가 사는 지역에서 펼쳐지는 경매장
-	</div>
-	<div class="content2">
-		<div class="Illust2">
-			<img src="@/assets/change2.jpg" style="width: 100%; height: 100%" />
-		</div>
+    <div class="Text2 content2TextFlex content2Title">
+        내가 사는 지역에서 펼쳐지는 경매장
+    </div>
+    <div class="content2">
+        <div class="Illust2">
+            <img src="@/assets/change2.jpg" style="width: 100%; height: 100%" />
+        </div>
 
-		<div class="Illust1">
-			<img src="@/assets/change.png" style="width: 100%; height: 100%" />
-		</div>
-	</div>
+        <div class="Illust1">
+            <img src="@/assets/change.png" style="width: 100%; height: 100%" />
+        </div>
+    </div>
 
-	<div class="Text1 content3Title">현재 경매 되고 있는 상품을 둘러보세요</div>
+    <div class="Text1 content3Title">현재 경매 되고 있는 상품을 둘러보세요</div>
 
-	<div class="content3 productFlex">
-		<div
-			class="productFlexItem"
-			v-for="item in productList"
-			:key="item.productNo"
-		>
-			<img class="ProductImg1" :src="item.productImage" />
+    <div class="content3 productFlex">
+        <div class="productFlexItem" v-for="item in productList" :key="item.productNo">
+            <img class="ProductImg1" :src="item.productImage" />
 
-			<div class="ProductN1">
-				<div class="ProductNT1">{{ item.productName }}</div>
-			</div>
+            <div class="ProductN1">
+                <div class="ProductNT1">{{ item.productName }}</div>
+            </div>
 
-			<div class="Adress1">
-				<div class="AdressT1">{{ item.wishAddr }}</div>
-				<!-- 상품거래지 -->
-			</div>
+            <div class="Adress1">
+                <div class="AdressT1">{{ item.wishAddr }}</div>
+                <!-- 상품거래지 -->
+            </div>
 
-			<div class="Price1">
-				<div class="PriceT1">{{ item.price }} 원</div>
-				<!-- 상품가격 -->
-			</div>
-		</div>
-	</div>
+            <div class="Price1">
+                <div class="PriceT1">{{ item.price }} 원</div>
+                <!-- 상품가격 -->
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-	import axios from '@/axios.js';
+import axios from '@/axios.js';
 
-	export default {
-		data() {
-			return {
-				productList: '',
-			};
-		},
+export default {
+    data() {
+        return {
+            productList: '',
+        };
+    },
 
-		methods: {
-			goLogin() {
-				this.$router.push('./login');
-			},
-			goAuction() {
-				this.$router.push('./auction');
-			},
-			goChat() {
-				this.$router.push('./chat');
-			},
+    methods: {
+        goLogin() {
+            this.$router.push('./login');
+        },
+        goAuction() {
+            this.$router.push('./auction');
+        },
+        goChat() {
+            this.$router.push('./chat');
+        },
 
-			product() {
-				axios.get('/product/main').then((response) => {
-					console.dir(response.data);
-					this.productList = response.data.list;
-				});
-			},
-			searchresultshow(keyword) {
-				if (keyword !== '') {
-					//검색어를 입력한 경우
-					this.$router.push({
-						name: 'SearchPage',
-						params: {
-							keyword: this.keyword,
-							isResultShow: true,
-						},
-					});
-					this.keyword = '';
-					console.log('"', keyword, '"' + ' 검색');
-				} else {
-					alert('검색어를 입력해주세요!'); //검색어를 입력하지 않은 경우
-				}
-			},
-		},
+        product() {
+            axios.get('/product/main').then((response) => {
+                console.dir(response.data);
+                this.productList = response.data.list;
+            });
+        },
+        searchresultshow(keyword) {
+            if (keyword !== '') {
+                //검색어를 입력한 경우
+                this.$router.push({
+                    name: 'SearchPage',
+                    params: {
+                        keyword: this.keyword,
+                        isResultShow: true,
+                    },
+                });
+                this.keyword = '';
+                console.log('"', keyword, '"' + ' 검색');
+            } else {
+                alert('검색어를 입력해주세요!'); //검색어를 입력하지 않은 경우
+            }
+        },
+    },
 
-		created() {
-			this.product();
-		},
-	};
+    created() {
+        this.product();
+    },
+};
 </script>
 
 <style scoped>
-	.productFlex {
-		display: flex;
-		width: 100%;
-		justify-content: center;
-	}
+.productFlex {
+    display: flex;
+    width: 100%;
+    justify-content: center;
+}
 
-	.productFlexItem {
-		margin-right: 30px;
-	}
+.productFlexItem {
+    margin-right: 30px;
+}
 
-	.content2TextFlex {
-		display: flex;
-		flex-direction: column;
-	}
+.content2TextFlex {
+    display: flex;
+    flex-direction: column;
+}
 
-	/* .productImgflexContainer {
+/* .productImgflexContainer {
 		justify-content: flex-end;
 		width: 100%;
 		display: flex;
 		flex-direction: column;
 	} */
 
-	.flexContainer {
-		justify-content: flex-end;
-		width: 100%;
-		display: flex;
-	}
+.flexContainer {
+    justify-content: flex-end;
+    width: 100%;
+    display: flex;
+}
 
-	.AuctionButton {
-		color: #0075ff;
-		text-align: center;
-		font: 700 18px 'Roboto', sans-serif;
-		position: absolute;
-		left: 575px;
-		top: 40px;
-		width: 40px;
-		height: 20px;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-	}
 
-	.Logo {
-		color: #0075ff;
-		text-align: center;
-		font: 700 24px 'Roboto', sans-serif;
-		position: absolute;
-		left: 395px;
-		top: 35px;
-		width: 110px;
-		height: 30px;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-	}
 
-	.ChatButton {
-		color: #000000;
-		text-align: center;
-		font: 700 18px 'Roboto', sans-serif;
-		position: absolute;
-		left: 655px;
-		top: 40px;
-		width: 40px;
-		height: 20px;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-	}
 
-	.SearchBox {
-		background: #d9ecff;
-		border-radius: 5px;
-		width: 450px;
-		height: 40px;
-		position: absolute;
-		left: 785px;
-		top: 30px;
-		text-align: center;
-		line-height: 40px;
-	}
 
-	.Search {
-		width: 130px;
-		height: 40px;
-		position: static;
-	}
 
-	.SearchButton {
-		background: #ffffff;
-		border-radius: 5px;
-		border-style: solid;
-		border-color: #000000;
-		border-width: 1px;
-		width: 130px;
-		height: 40px;
-		position: absolute;
-		left: 1265px;
-		top: 30px;
-	}
 
-	.SearchText {
-		width: 80px;
-		height: 30px;
-		position: absolute;
-		left: 1290px;
-		top: 35px;
-	}
+.content1 {
+    background: #f1f8ff;
+    height: 600px;
+}
 
-	.SearchText1 {
-		color: #000000;
-		text-align: center;
-		font: 700 18px 'Roboto', sans-serif;
-		position: absolute;
-		left: 0px;
-		top: 0px;
-		width: 80px;
-		height: 30px;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-	}
+.content2 {
+    background: #f3f4ff;
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+    height: 350px;
+}
 
-	.Login {
-		width: 80px;
-		height: 40px;
-		position: static;
-	}
+.content2Title {
+    background: #f3f4ff;
+    justify-content: center;
+    width: 100%;
+}
 
-	.LoginBox {
-		background: #0075ff;
-		border-radius: 5px;
-		width: 80px;
-		height: 40px;
-		position: absolute;
-		left: 1445px;
-		top: 30px;
-	}
+.content3 {
+    background: #f6efff;
+    height: 400px;
+}
 
-	.LoginText {
-		width: 80px;
-		height: 30px;
-		position: absolute;
-		left: 1445px;
-		top: 35px;
-	}
+.content3Title {
+    background: #f6efff;
+    width: 100%;
+    justify-content: center;
+}
 
-	.LoginText1 {
-		color: #ffffff;
-		text-align: center;
-		font: 700 18px 'Roboto', sans-serif;
-		position: absolute;
-		left: 0px;
-		top: 0px;
-		width: 80px;
-		height: 30px;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-	}
+.Text1 {
+    color: #000000;
+    text-align: center;
+    font: 700 33px 'Roboto', sans-serif;
+    height: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
 
-	.content1 {
-		background: #f1f8ff;
-		height: 600px;
-	}
+.Text2 {
+    color: #000000;
+    text-align: center;
+    font: 700 35px 'Roboto', sans-serif;
+    height: 100px;
+    align-items: center;
+}
 
-	.content2 {
-		background: #f3f4ff;
-		width: 100%;
-		display: flex;
-		justify-content: space-evenly;
-		height: 350px;
-	}
+.Text3 {
+    color: #000000;
+    text-align: left;
+    font: 700 35px 'Roboto', sans-serif;
+    align-items: center;
+    position: absolute;
+    left: 15%;
+    top: 40%;
+}
 
-	.content2Title {
-		background: #f3f4ff;
-		justify-content: center;
-		width: 100%;
-	}
+.Text4 {
+    color: #000000;
+    text-align: left;
+    font: 400 16px 'Roboto', sans-serif;
+    align-items: center;
+    justify-content: flex-start;
+}
 
-	.content3 {
-		background: #f6efff;
-		height: 400px;
-	}
+.topImages {
+    position: absolute;
+    width: 800px;
+    height: 450px;
+    right: 10%;
+}
 
-	.content3Title {
-		background: #f6efff;
-		width: 100%;
-		justify-content: center;
-	}
 
-	.Text1 {
-		color: #000000;
-		text-align: center;
-		font: 700 33px 'Roboto', sans-serif;
-		height: 100px;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-	}
 
-	.Text2 {
-		color: #000000;
-		text-align: center;
-		font: 700 35px 'Roboto', sans-serif;
-		height: 100px;
-		align-items: center;
-	}
 
-	.Text3 {
-		color: #000000;
-		text-align: left;
-		font: 700 35px 'Roboto', sans-serif;
-		align-items: center;
-		position: absolute;
-		left: 15%;
-		top: 40%;
-	}
+.Illust2 {
+    background: #bdbdbd;
+    width: 500px;
+    height: 270px;
 
-	.Text4 {
-		color: #000000;
-		text-align: left;
-		font: 400 16px 'Roboto', sans-serif;
-		align-items: center;
-		justify-content: flex-start;
-	}
+    box-shadow: 5px 4px 4px 0px rgba(0, 0, 0, 0.25);
+}
 
-	.topImages {
-		position: absolute;
-		width: 800px;
-		height: 190px;
-		right: 10%;
-	}
+.Illust1 {
+    background: #bdbdbd;
+    width: 500px;
+    height: 270px;
 
-	.rectangle-12 {
-		background: #bdbdbd;
-		width: 400px;
-		height: 190px;
-		box-shadow: 5px 4px 4px 0px rgba(0, 0, 0, 0.25);
-		right: 38%;
-		top: 30%;
-	}
+    box-shadow: 5px 4px 4px 0px rgba(0, 0, 0, 0.25);
+}
 
-	.rectangle-13 {
-		background: #bdbdbd;
-		width: 400px;
-		height: 190px;
-		box-shadow: 5px 4px 4px 0px rgba(0, 0, 0, 0.25);
-		left: 45%;
-		top: 50%;
-	}
+.ProductN1 {
+    width: 133px;
+    height: 20px;
+}
 
-	.rectangle-14 {
-		background: #bdbdbd;
-		width: 400px;
-		height: 190px;
-		box-shadow: 5px 4px 4px 0px rgba(0, 0, 0, 0.25);
-		left: 58%;
-		top: 35%;
-	}
+.ProductNT1 {
+    color: #000000;
+    text-align: left;
+    font: 400 16px 'Roboto', sans-serif;
 
-	.ProductImg1 {
-		background: #bdbdbd;
-		border-radius: 5px;
-		width: 240px;
-		height: 180px;
-	}
+    width: 240px;
+    height: 20px;
+    align-items: center;
+    justify-content: flex-start;
+}
 
-	.Illust2 {
-		background: #bdbdbd;
-		width: 500px;
-		height: 270px;
+.Adress1 {
+    width: 240px;
+    height: 20px;
+}
 
-		box-shadow: 5px 4px 4px 0px rgba(0, 0, 0, 0.25);
-	}
+.AdressT1 {
+    color: #000000;
+    text-align: left;
+    font: 400 16px 'Roboto', sans-serif;
 
-	.Illust1 {
-		background: #bdbdbd;
-		width: 500px;
-		height: 270px;
+    width: 240px;
+    height: 20px;
+    align-items: center;
+    justify-content: flex-start;
+}
 
-		box-shadow: 5px 4px 4px 0px rgba(0, 0, 0, 0.25);
-	}
+.Price1 {
+    width: 133px;
+    height: 20px;
+    left: 320px;
+    top: 1705px;
+}
 
-	.ProductN1 {
-		width: 133px;
-		height: 20px;
-	}
+.PriceT1 {
+    color: #000000;
+    text-align: left;
+    font: 700 16px 'Roboto', sans-serif;
 
-	.ProductNT1 {
-		color: #000000;
-		text-align: left;
-		font: 400 16px 'Roboto', sans-serif;
+    width: 133px;
+    height: 20px;
+    align-items: center;
+    justify-content: flex-start;
+}
 
-		width: 240px;
-		height: 20px;
-		align-items: center;
-		justify-content: flex-start;
-	}
 
-	.Adress1 {
-		width: 240px;
-		height: 20px;
-	}
 
-	.AdressT1 {
-		color: #000000;
-		text-align: left;
-		font: 400 16px 'Roboto', sans-serif;
-
-		width: 240px;
-		height: 20px;
-		align-items: center;
-		justify-content: flex-start;
-	}
-
-	.Price1 {
-		width: 133px;
-		height: 20px;
-		left: 320px;
-		top: 1705px;
-	}
-
-	.PriceT1 {
-		color: #000000;
-		text-align: left;
-		font: 700 16px 'Roboto', sans-serif;
-
-		width: 133px;
-		height: 20px;
-		align-items: center;
-		justify-content: flex-start;
-	}
-
-	.----11 {
-		color: #000000;
-		text-align: left;
-		font: 700 16px 'Roboto', sans-serif;
-		position: absolute;
-
-		width: 134px;
-		height: 20px;
-		display: flex;
-		align-items: center;
-		justify-content: flex-start;
-	}
-
-	* {
-		padding: 0;
-		margin: 0;
-	}
+* {
+    padding: 0;
+    margin: 0;
+}
 </style>
--- front/anabadaonline/src/components/member/login/JoinMember.vue
+++ front/anabadaonline/src/components/member/login/JoinMember.vue
@@ -1,244 +1,244 @@
 <template>
-	<div class="flexContainer">
-		<form @submit.prevent="submitForm">
-			<div class="text">본인 정보를 입력해 주세요</div>
-			<div class="id">
-				<label for="id">id</label>
-				<input type="text" id="id" v-model="id" />
-			</div>
-			<div class="pw">
-				<label for="pw">pw</label>
-				<input type="password" id="pw" v-model="pw" />
-			</div>
-			<div class="name">
-				<label for="name">이름</label>
-				<input type="text" id="name" v-model="name" />
-			</div>
-			<div class="birth">
-				<label for="birth">생년월일</label>
-				<input type="date" id="birth" v-model="birth" />
-			</div>
-			<div class="addr">
-				<label for="addr">주소</label>
-				<input type="text" id="addr" v-model="addr" />
-			</div>
-			<div class="detailaddr">
-				<label for="detailaddr">상세주소</label>
-				<input type="text" id="detailaddr" v-model="detailaddr" />
-			</div>
-			<div class="Wishaddr">
-				<label for="Wishaddr">거래희망지</label>
-				<input type="text" id="Wishaddr" v-model="Wishaddr" />
-			</div>
-			<div class="image">
-				<label for="image">프로필 이미지</label>
-				<input id="image" type="file" @change="onInputImage" />
-			</div>
+    <div class="flexContainer">
+        <form @submit.prevent="submitForm">
+            <div class="text">본인 정보를 입력해 주세요</div>
+            <div class="id">
+                <label for="id">id</label>
+                <input type="text" id="id" v-model="id" />
+            </div>
+            <div class="pw">
+                <label for="pw">pw</label>
+                <input type="password" id="pw" v-model="pw" />
+            </div>
+            <div class="name">
+                <label for="name">이름</label>
+                <input type="text" id="name" v-model="name" />
+            </div>
+            <div class="birth">
+                <label for="birth">생년월일</label>
+                <input type="date" id="birth" v-model="birth" />
+            </div>
+            <div class="addr">
+                <label for="addr">주소</label>
+                <input type="text" id="addr" v-model="addr" />
+            </div>
+            <div class="detailaddr">
+                <label for="detailaddr">상세주소</label>
+                <input type="text" id="detailaddr" v-model="detailaddr" />
+            </div>
+            <div class="Wishaddr">
+                <label for="Wishaddr">거래희망지</label>
+                <input type="text" id="Wishaddr" v-model="Wishaddr" />
+            </div>
+            <div class="image">
+                <label for="image">프로필 이미지</label>
+                <input id="image" type="file" @change="onInputImage" />
+            </div>
 
-			<button type="submit" class="join">회원가입</button>
-			<button class="cancel" @click="goLogin">취소하기</button>
-		</form>
-	</div>
+            <button type="submit" class="join">회원가입</button>
+            <button class="cancel" @click="goLogin">취소하기</button>
+        </form>
+    </div>
 </template>
 
 <script>
-	import axios from '@/axios';
-	export default {
-		name: 'SignupForm',
-		data() {
-			return {
-				id: '',
-				pw: '',
-				name: '',
-				birth: '',
-				detailaddr: '',
-				addr: '',
-				Wishaddr: '',
-				image: '',
-			};
-		},
-		methods: {
-			submitForm() {
-				let form = new FormData();
-				form.append('id', this.id);
-				form.append('pw', this.pw);
-				form.append('name', this.name);
-				// form.append("birth",this.birth)
-				form.append('detailAddr', this.detailaddr);
-				form.append('addr', this.addr);
-				form.append('wishAddr', this.Wishaddr);
+import axios from '@/axios';
+export default {
+    name: 'SignupForm',
+    data() {
+        return {
+            id: '',
+            pw: '',
+            name: '',
+            birth: '',
+            detailaddr: '',
+            addr: '',
+            Wishaddr: '',
+            image: '',
+        };
+    },
+    methods: {
+        submitForm() {
+            let form = new FormData();
+            form.append('id', this.id);
+            form.append('pw', this.pw);
+            form.append('name', this.name);
+            // form.append("birth",this.birth)
+            form.append('detailAddr', this.detailaddr);
+            form.append('addr', this.addr);
+            form.append('wishAddr', this.Wishaddr);
 
-				axios
-					.post('/member', form, {
-						header: { 'Content-Type': 'multipart/form-data' },
-					})
-					.then((response) => {
-						console.log(response);
-						if (response.status == 200) {
-							// 로그인 하면 토큰 발급
-							this.$router.push('./login');
-						}
-					});
-			},
-			onInputImage(e) {
-				this.image = e.target.files[0];
-			},
-			goLogin() {
-				this.$router.push('./login');
-			},
-		},
-	};
+            axios
+                .post('/member', form, {
+                    header: { 'Content-Type': 'multipart/form-data' },
+                })
+                .then((response) => {
+                    console.log(response);
+                    if (response.status == 200) {
+                        // 로그인 하면 토큰 발급
+                        this.$router.push('./login');
+                    }
+                });
+        },
+        onInputImage(e) {
+            this.image = e.target.files[0];
+        },
+        goLogin() {
+            this.$router.push('./login');
+        },
+    },
+};
 </script>
 
 <style scoped>
-	.id {
-		color: #000000;
-		text-align: left;
-		font: 400 16px 'Roboto', sans-serif;
-		position: absolute;
-		left: 648px;
-		top: 211px;
-		width: 398px;
-		height: 37px;
-		display: flex;
-		align-items: center;
-		justify-content: flex-start;
-	}
+.id {
+    color: #000000;
+    text-align: left;
+    font: 400 16px 'Roboto', sans-serif;
+    position: absolute;
+    left: 648px;
+    top: 211px;
+    width: 398px;
+    height: 37px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+}
 
-	.text {
-		color: #0075ff;
-		text-align: left;
-		font: 700 28px 'Roboto', sans-serif;
-		position: absolute;
-		top: 130px;
-		width: 398px;
-		height: 37px;
-		display: flex;
-		align-items: center;
-		justify-content: flex-start;
-	}
+.text {
+    color: #0075ff;
+    text-align: left;
+    font: 700 28px 'Roboto', sans-serif;
+    position: absolute;
+    top: 130px;
+    width: 398px;
+    height: 37px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+}
 
-	.pw {
-		color: #000000;
-		text-align: left;
-		font: 400 16px 'Roboto', sans-serif;
-		position: absolute;
-		left: 648px;
-		top: 273px;
-		width: 398px;
-		height: 37px;
-		display: flex;
-		align-items: center;
-		justify-content: flex-start;
-	}
+.pw {
+    color: #000000;
+    text-align: left;
+    font: 400 16px 'Roboto', sans-serif;
+    position: absolute;
+    left: 648px;
+    top: 273px;
+    width: 398px;
+    height: 37px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+}
 
-	.name {
-		color: #000000;
-		text-align: left;
-		font: 400 16px 'Roboto', sans-serif;
-		position: absolute;
-		left: 648px;
-		top: 337px;
-		width: 398px;
-		height: 37px;
-		display: flex;
-		align-items: center;
-		justify-content: flex-start;
-	}
+.name {
+    color: #000000;
+    text-align: left;
+    font: 400 16px 'Roboto', sans-serif;
+    position: absolute;
+    left: 648px;
+    top: 337px;
+    width: 398px;
+    height: 37px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+}
 
-	.birth {
-		color: #000000;
-		text-align: left;
-		font: 400 16px 'Roboto', sans-serif;
-		position: absolute;
-		left: 648px;
-		top: 401px;
-		width: 398px;
-		height: 37px;
-		display: flex;
-		align-items: center;
-		justify-content: flex-start;
-	}
+.birth {
+    color: #000000;
+    text-align: left;
+    font: 400 16px 'Roboto', sans-serif;
+    position: absolute;
+    left: 648px;
+    top: 401px;
+    width: 398px;
+    height: 37px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+}
 
-	.addr {
-		color: #000000;
-		text-align: left;
-		font: 400 16px 'Roboto', sans-serif;
-		position: absolute;
-		left: 648px;
-		top: 465px;
-		width: 398px;
-		height: 37px;
-		display: flex;
-		align-items: center;
-		justify-content: flex-start;
-	}
+.addr {
+    color: #000000;
+    text-align: left;
+    font: 400 16px 'Roboto', sans-serif;
+    position: absolute;
+    left: 648px;
+    top: 465px;
+    width: 398px;
+    height: 37px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+}
 
-	.detailaddr {
-		color: #000000;
-		text-align: left;
-		font: 400 16px 'Roboto', sans-serif;
-		position: absolute;
-		left: 648px;
-		top: 529px;
-		width: 398px;
-		height: 37px;
-		display: flex;
-		align-items: center;
-		justify-content: flex-start;
-	}
+.detailaddr {
+    color: #000000;
+    text-align: left;
+    font: 400 16px 'Roboto', sans-serif;
+    position: absolute;
+    left: 648px;
+    top: 529px;
+    width: 398px;
+    height: 37px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+}
 
-	.Wishaddr {
-		color: #000000;
-		text-align: left;
-		font: 400 16px 'Roboto', sans-serif;
-		position: absolute;
-		left: 648px;
-		top: 593px;
-		width: 398px;
-		height: 37px;
-		display: flex;
-		align-items: center;
-		justify-content: flex-start;
-	}
+.Wishaddr {
+    color: #000000;
+    text-align: left;
+    font: 400 16px 'Roboto', sans-serif;
+    position: absolute;
+    left: 648px;
+    top: 593px;
+    width: 398px;
+    height: 37px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+}
 
-	.image {
-		color: #000000;
-		text-align: left;
-		font: 400 16px 'Roboto', sans-serif;
-		position: absolute;
-		left: 648px;
-		top: 657px;
-		width: 398px;
-		height: 37px;
-		display: flex;
-		align-items: center;
-		justify-content: flex-start;
-	}
+.image {
+    color: #000000;
+    text-align: left;
+    font: 400 16px 'Roboto', sans-serif;
+    position: absolute;
+    left: 648px;
+    top: 657px;
+    width: 398px;
+    height: 37px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+}
 
-	.join {
-		width: 8.5rem;
-		height: 50px;
-		line-height: 50px;
-		background-color: white;
-		border-radius: 5px;
-		margin: 0 auto;
-		color: #0075ff;
-		position: absolute;
-		left: 648px;
-		top: 727px;
-	}
+.join {
+    width: 8.5rem;
+    height: 50px;
+    line-height: 50px;
+    background-color: white;
+    border-radius: 5px;
+    margin: 0 auto;
+    color: #0075ff;
+    position: absolute;
+    left: 648px;
+    top: 727px;
+}
 
-	.cancel {
-		width: 8.5rem;
-		height: 50px;
-		line-height: 50px;
-		background-color: white;
-		border-radius: 5px;
-		margin: 0 auto;
-		color: #e20303;
-		position: absolute;
-		left: 1000px;
-		top: 727px;
-	}
+.cancel {
+    width: 8.5rem;
+    height: 50px;
+    line-height: 50px;
+    background-color: white;
+    border-radius: 5px;
+    margin: 0 auto;
+    color: #e20303;
+    position: absolute;
+    left: 1000px;
+    top: 727px;
+}
 </style>
