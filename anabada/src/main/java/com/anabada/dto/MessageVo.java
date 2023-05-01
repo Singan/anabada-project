9f42879b7232799bd4c17d54e6b8e6b949f2d471
석성희 <sunghee119z@naver.com>
2023-05-01 19:30:28 +0900

    d

--- anabada/src/main/java/com/anabada/dto/MessageVo.java
+++ anabada/src/main/java/com/anabada/dto/MessageVo.java
@@ -1,9 +1,11 @@
 package com.anabada.dto;
 
 import lombok.Data;
+import lombok.ToString;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @Data
+@ToString
 public class MessageVo {
     private String memberId;
     private String message;
