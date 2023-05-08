13db40be8ccd1c3bfb420a74b46174df636e7688
Hwi <124661622+Imhwitae@users.noreply.github.com>
2023-05-08 16:53:25 +0900

    상품 전체 리스트에 카테고리 추가

--- anabada/src/main/java/com/anabada/dto/response_dto/ProductFindAllDto.java
+++ anabada/src/main/java/com/anabada/dto/response_dto/ProductFindAllDto.java
@@ -1,7 +1,10 @@
 package com.anabada.dto.response_dto;
 
+import com.anabada.entity.Category;
 import com.anabada.entity.Product;
 import lombok.*;
+
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -13,12 +16,14 @@ public class ProductFindAllDto {
     private String wishAddr;
     private Integer price;
     private String productImage;
+    private Category category;
     // 카테고리 넣기
     public ProductFindAllDto(Product product) {
         this.productName = (product.getProductName());
         this.productNo = (product.getProductNo());
         this.wishAddr = (product.getMember().getMemberWishAddr());
         this.price = (product.getProductPrice());
+        this.category = product.getCategory();
         if(!product.getProductImageList().isEmpty()) {
             this.productImage = (product.getProductImageList().get(0).getImageAddr());
         }
