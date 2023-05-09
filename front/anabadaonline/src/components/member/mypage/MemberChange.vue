d71b3c0629ecc05b66d0fe4460e05e6e4805bd80
seung-min-0109 <127377279+seung-min-0109@users.noreply.github.com>
2023-05-09 22:41:32 +0900

    회원정보수정 하는중

--- front/anabadaonline/src/components/member/mypage/MemberChange.vue
+++ front/anabadaonline/src/components/member/mypage/MemberChange.vue
@@ -6,44 +6,36 @@
 			</div>
 		</div>
 
-		<div class="--">
-			<div class="----">아이디</div>
+		<div class="id">
+			<div class="Text">아이디</div>
 		</div>
 
-		<div class="----------">
-			<div class="rectangle-27"></div>
+		<div class="idBox"></div>
 
-			<div class="--4">
-				<div class="----2">abcd@gmail.com</div>
-			</div>
+		<div class="--4">
+			<div class="----2">abcd@gmail.com</div>
 		</div>
 
-		<div class="--5">
-			<div class="----">이름</div>
+		<div class="name">
+			<div class="Text">이름</div>
 		</div>
 
-		<div class="----------">
-			<div class="rectangle-272"></div>
+		<div class="nameBox"></div>
 
-			<div class="--6">
-				<div class="----2">홍길동</div>
-			</div>
+		<div class="--6">
+			<div class="----2">홍길동</div>
 		</div>
 
 		<div class="--7">
-			<div class="----">비밀번호 변경</div>
+			<div class="Text">비밀번호 변경</div>
 		</div>
 
-		<div class="----------">
-			<div class="rectangle-273"></div>
+		<div class="rectangle-273"></div>
 
-			<div class="--8"></div>
-
-			<div class="----3">현재 비밀번호를 입력하세요</div>
-		</div>
+		<div class="----3">현재 비밀번호를 입력하세요</div>
 
 		<div class="--9">
-			<div class="----">주소 변경</div>
+			<div class="Text">주소 변경</div>
 		</div>
 
 		<div class="----------2">
@@ -62,23 +54,17 @@
 			<div class="-----">주소 찾기</div>
 		</div>
 
-		<div class="----------">
-			<div class="rectangle-276"></div>
+		<div class="rectangle-276"></div>
 
-			<div class="--12"></div>
+		<div class="--12"></div>
 
-			<div class="-------------">새 비밀번호를 입력하세요</div>
-		</div>
+		<div class="-------------">새 비밀번호를 입력하세요</div>
 
-		<div class="----------">
-			<div class="rectangle-277"></div>
+		<div class="rectangle-277"></div>
 
-			<div class="--13"></div>
+		<div class="--13"></div>
 
-			<div class="------------------">
-				새 비밀번호를 한 번더 입력하세요
-			</div>
-		</div>
+		<div class="------------------">새 비밀번호를 한 번더 입력하세요</div>
 
 		<div class="--14">
 			<div class="rectangle-25"></div>
@@ -129,7 +115,7 @@
 		top: 40px;
 	}
 
-	.-- {
+	.id {
 		width: 370px;
 		height: 30px;
 		position: absolute;
@@ -137,7 +123,7 @@
 		top: 168px;
 	}
 
-	.---- {
+	.Text {
 		color: #000000;
 		text-align: left;
 		font: 400 16px 'Roboto', sans-serif;
@@ -157,7 +143,7 @@
 		position: static;
 	}
 
-	.rectangle-27 {
+	.idBox {
 		background: #e1e1e1;
 		border-radius: 5px;
 		border-style: solid;
@@ -192,7 +178,7 @@
 		justify-content: flex-start;
 	}
 
-	.--5 {
+	.name {
 		width: 370px;
 		height: 30px;
 		position: absolute;
@@ -200,7 +186,7 @@
 		top: 280px;
 	}
 
-	.rectangle-272 {
+	.nameBox {
 		background: #e1e1e1;
 		border-radius: 5px;
 		border-style: solid;
