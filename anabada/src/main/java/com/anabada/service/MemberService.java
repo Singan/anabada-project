a97684cf00b51b3b946686514e8326c4079242a6
석성희 <sunghee119z@naver.com>
2023-05-16 23:28:05 +0900

    마이페이지 가기 비밀번호 체크

--- anabada/src/main/java/com/anabada/service/MemberService.java
+++ anabada/src/main/java/com/anabada/service/MemberService.java
@@ -104,8 +104,9 @@ public class MemberService implements UserDetailsService {
     public boolean confirmPassword(MemberDetailDTO memberDetailDTO, MypageConfirmDto mypageConfirmDto) {
         Member member = memberRepository.findByMemberId(memberDetailDTO.getUsername());
         String confirmPw = mypageConfirmDto.getConfirmPassword();
-
-        if (member.getMemberPw().equals(confirmPw)) {
+        System.out.println(confirmPw);
+        System.out.println(passwordEncoder.matches( confirmPw,member.getMemberPw()));
+        if (passwordEncoder.matches( confirmPw,member.getMemberPw())) {
             return true;
         } else {
             throw new RuntimeException("비밀번호가 다릅니다.");
