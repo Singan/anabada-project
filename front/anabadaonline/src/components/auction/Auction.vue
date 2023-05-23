fe48f3b6ad1f3a343ac0887a91a1b6c117b9b695
석성희 <sunghee119z@naver.com>
2023-05-23 23:42:24 +0900

    상품목록 뷰 수정

--- front/anabadaonline/src/components/auction/Auction.vue
+++ front/anabadaonline/src/components/auction/Auction.vue
@@ -1,22 +1,24 @@
 <template>
-	<div class="title">
-		<select v-model="categoryNum" class="title">
-			<option v-for="item in categoryList" :key="item.categoryNo" :value="item.categoryNo" class="title">
-				<div class="title">{{ item.categoryName }}</div>
-			</option>
-		</select>
-		<button @click="product" class="list">조회</button>
+	<div class="titleBox">
+		<div
+			v-for="item in categoryList"
+			:key="item.categoryNo"
+			:value="item.categoryNo"
+			class="title"
+			@click="product(item.categoryNo)"
+		>
+			{{ item.categoryName }}
+		</div>
 	</div>
 	<div class="productFlex">
 		<div class="flexItem" v-for="item in productList" :key="item.productNo">
 			<router-link :to="'/ProductDt?productNo=' + item.productNo">
-				<img class="productImg" :src="item.productImage" style="width: 160px; height: 160px" />
+				<img class="productImg" :src="item.productImage" />
 			</router-link>
 
 			<div class="productName">{{ item.productName }}</div>
 
 			<div class="price">등록 가격 : {{ item.productPrice }} 원</div>
-			<div class="area">{{ item.productNo }}</div>
 		</div>
 	</div>
 </template>
@@ -30,7 +32,6 @@
 		data() {
 			return {
 				productList: '',
-				categoryNum: 1, //초기값
 				categoryList: '',
 
 				categoryNo: '',
@@ -43,8 +44,8 @@
 					this.categoryList = response.data;
 				});
 			},
-			product() {
-				axios.get('/product/list?categoryNo=' + this.categoryNum).then((response) => {
+			product(key) {
+				axios.get('/product/list?categoryNo=' + key).then((response) => {
 					console.dir(response.data);
 					this.productList = response.data;
 				});
@@ -53,7 +54,7 @@
 		mounted() {},
 
 		created() {
-			this.product();
+			this.product(1);
 			this.category();
 		},
 	};
@@ -63,191 +64,61 @@
 		margin: auto;
 		display: flex;
 		width: 60%;
-		justify-content: center;
 		align-items: center;
 		flex-wrap: wrap;
+		justify-content: flex-start;
 	}
 
 	.flexItem {
-		margin-right: 70px;
-		margin-bottom: 40px;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		width: 20%;
+		margin: 5%;
 	}
 
-	.title {
-		color: #000000;
+	.titleBox {
+		color: #010101;
 		text-align: center;
-		font: 600 24px 'Roboto', sans-serif;
+		background-color: skyblue;
+		font: 600 18px 'Roboto', sans-serif;
 		margin: 50px auto;
-		left: 825px;
-		top: 161px;
-		width: 270px;
+		width: 60%;
 		height: 39px;
 		display: flex;
 		align-items: center;
 		justify-content: center;
-		border: none;
 	}
-
+	.title {
+		border: 1px black solid;
+		width: 100%;
+		height: calc(100% - 1px);
+		justify-content: center;
+		align-items: center;
+		display: flex;
+	}
 	.productImg {
 		background: #d9d9d9;
 		border-radius: 20px;
-		width: 160px;
-		height: 160px;
-
-		left: 384px;
-		top: 285px;
+		width: 100%;
+		height: 100%;
 	}
 
 	.productName {
 		color: #000000;
-		text-align: left;
+		text-align: center;
 		font: 300 16px 'Roboto', sans-serif;
-
-		left: 384px;
-		top: 445px;
-		width: 160px;
-		height: 23px;
-
-		align-items: center;
-		justify-content: flex-start;
+		width: 100%;
+		height: 100%;
 	}
 
 	.price {
 		color: #000000;
-		text-align: left;
+		text-align: center;
 		font: 600 16px 'Roboto', sans-serif;
 
-		left: 387px;
-		top: 473px;
-		width: 157px;
-		height: 23px;
-
-		align-items: center;
-		justify-content: flex-start;
-	}
-
-	.area {
-		color: #000000;
-		text-align: left;
-		font: 400 16px 'Roboto', sans-serif;
-
-		left: 387px;
-		top: 501px;
-		width: 157px;
-		height: 23px;
-
-		align-items: center;
-		justify-content: flex-start;
-	}
-
-	.interest {
-		color: #797979;
-		text-align: left;
-		font: 400 13px 'Roboto', sans-serif;
-		position: absolute;
-		left: 386.67px;
-		top: 524px;
-		width: 38.67px;
-		height: 23px;
-		display: flex;
-		align-items: center;
-		justify-content: flex-start;
-	}
-
-	.chat {
-		color: #797979;
-		text-align: left;
-		font: 400 13px 'Roboto', sans-serif;
-		position: absolute;
-		left: 470.67px;
-		top: 524px;
-		width: 40px;
-		height: 23px;
-		display: flex;
-		align-items: center;
-		justify-content: flex-start;
-	}
-
-	.chatCount {
-		color: #797979;
-		text-align: left;
-		font: 400 13px 'Roboto', sans-serif;
-		position: absolute;
-		left: 520px;
-		top: 524px;
-		width: 24px;
-		height: 23px;
-		display: flex;
-		align-items: center;
-		justify-content: flex-start;
-	}
-
-	.interestCount {
-		color: #797979;
-		text-align: left;
-		font: 400 13px 'Roboto', sans-serif;
-		position: absolute;
-		left: 425.33px;
-		top: 524px;
-		width: 40px;
-		height: 23px;
-		display: flex;
-		align-items: center;
-		justify-content: flex-start;
-	}
-
-	.rectangle-202 {
-		background: #ffffff;
-		width: 160px;
-		height: 260px;
-		position: absolute;
-		left: 385px;
-		top: 630px;
-	}
-
-	.rectangle-162 {
-		background: #d9d9d9;
-		border-radius: 20px;
-		width: 160px;
-		height: 160px;
-		position: absolute;
-		left: 384px;
-		top: 630px;
-	}
-
-	.rectangle-203 {
-		background: #ffffff;
-		width: 160px;
-		height: 260px;
-		position: absolute;
-		left: 705px;
-		top: 285px;
-	}
-
-	.rectangle-163 {
-		background: #d9d9d9;
-		border-radius: 20px;
-		width: 160px;
-		height: 160px;
-		position: absolute;
-		left: 703px;
-		top: 285px;
-	}
-
-	.rectangle-204 {
-		position: absolute;
-		left: 700.5px;
-		top: 630px;
-		overflow: visible;
-	}
-
-	.rectangle-164 {
-		background: #d9d9d9;
-		border-radius: 20px;
-		width: 160px;
-		height: 160px;
-		position: absolute;
-		left: 700px;
-		top: 630px;
+		width: 100%;
+		height: 100%;
 	}
 </style>
