1c858e01aef27dcd568261ba0e76014c7a06fa8c
석성희 <sunghee119z@naver.com>
2023-05-27 23:58:31 +0900

    무한스크롤

--- front/anabadaonline/src/components/auction/Auction.vue
+++ front/anabadaonline/src/components/auction/Auction.vue
@@ -11,7 +11,7 @@
 			{{ item.categoryName }}
 		</router-link>
 	</div>
-	<div class="productFlex" ref="productFlex">
+	<div class="productFlex">
 		<div class="flexItem" v-for="item in productList" :key="item.productNo">
 			<router-link :to="'/ProductDt?productNo=' + item.productNo">
 				<img class="productImg" :src="item.productImage" />
@@ -27,22 +27,9 @@
 	import axios from '@/axios.js';
 
 	export default {
-		watch: {
-			categoryNo(newCategoryNo) {
-				(this.page = 0), (this.totalCountPage = 0);
-
-				console.log('바뀜?');
-				this.product();
-			},
-		},
 		name: 'Auction',
 		components: {},
 		props: {},
-		computed: {
-			categoryNo() {
-				return this.$route.query.category || 1;
-			},
-		},
 		data() {
 			return {
 				productList: [],
@@ -50,13 +37,19 @@
 				size: 8,
 				page: 0,
 				totalCountPage: 0,
+				loading: false, // 데이터 로딩 상태 추가
+				debounceTimer: null, // 딜레이 타이머 변수 추가
 			};
+		},
+		computed: {
+			categoryNo() {
+				return this.$route.query.category || 1;
+			},
 		},
 		methods: {
 			category() {
 				axios.get('/category').then((response) => {
 					this.categoryList = response.data;
-					this.product();
 				});
 			},
 			async product() {
@@ -65,32 +58,57 @@
 				);
 				this.productList = response.data.list;
 				this.totalCountPage = response.data.totalCountPage;
+				this.loading = false; // 데이터 로딩 완료 후 상태 변경
 			},
 			scroll() {
-				const element = this.$refs.productFlex;
+				// 딜레이 시간 설정
+				const delay = 200;
 
-				const scrollBottom = element.scrollTop + element.clientHeight;
-				// 스크롤 위치 확인
-				if (scrollBottom >= element.scrollHeight) {
-					this.loadMoreData();
+				// 딜레이 타이머가 존재할 경우 이전 타이머 제거
+				if (this.debounceTimer) {
+					clearTimeout(this.debounceTimer);
 				}
+
+				// 딜레이 후 로딩 함수 호출
+				this.debounceTimer = setTimeout(() => {
+					const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+					const windowHeight = window.innerHeight;
+					const documentHeight = document.documentElement.scrollHeight;
+
+					// 스크롤 위치 확인 및 로딩 상태 확인
+					if (scrollPosition + windowHeight >= documentHeight && !this.loading) {
+						this.loadMoreData();
+					}
+				}, delay);
 			},
 
 			async loadMoreData() {
+				this.loading = true;
+
 				// 추가 데이터 로딩
 				if (this.page < this.totalCountPage) {
-					console.log('추가 데이터 가져오기');
+					console.log('데이터호출');
 					this.page++;
 					let response = await axios.get(
 						'/product/list?categoryNo=' + this.categoryNo + '&&size=' + this.size + '&&page=' + this.page,
 					);
 					this.productList.push(...response.data.list);
 				}
+
+				// 로딩 상태 변경
+				this.loading = false;
+			},
+			scrollToTop() {
+				window.scrollTo({
+					top: 0,
+					behavior: 'smooth',
+				});
 			},
 		},
 		mounted() {
 			// scroll 이벤트 핸들러 등록
 			window.addEventListener('scroll', this.scroll);
+			this.scrollToTop();
 		},
 		beforeUnmount() {
 			// scroll 이벤트 핸들러 제거
@@ -98,6 +116,7 @@
 		},
 		created() {
 			this.category();
+			this.product();
 		},
 	};
 </script>
@@ -106,7 +125,7 @@
 		margin: auto;
 		display: grid;
 		width: 60%;
-		row-gap: 10px;
+		row-gap: 50px;
 		/* row의 간격을 10px로 */
 		column-gap: 20px;
 		/* column의 간격을 20px로 */
