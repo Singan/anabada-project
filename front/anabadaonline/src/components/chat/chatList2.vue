1cfe07ffdf7ccf3aa884a7b8fe9bc7c5302f195d
simsimhang <seajin97@g.shingu.ac.kr>
2023-06-08 22:24:59 +0900

    d
    d

---
+++ front/anabadaonline/src/components/chat/chatList2.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="form">
+		<div>채팅리스트를 다시 만들어봅시다</div>
+		<div>채팅리스트를 다시 만들어봅시다</div>
+		<div>채팅리스트를 다시 만들어봅시다</div>
+		<div>채팅리스트를 다시 만들어봅시다</div>
+	</div>
+</template>
+
+<script>
+	export default {};
+</script>
+
+<style scoped>
+	.form {
+		display: flex;
+		flex-direction: column;
+		background-color: antiquewhite;
+		width: 100%;
+		height: 100%;
+	}
+</style>
