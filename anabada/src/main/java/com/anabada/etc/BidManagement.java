e02dda9799d5b9f1da75f97df91eb37702fa362d
석성희 <sunghee119z@naver.com>
2023-06-14 20:59:56 +0900

    ㅇ

--- anabada/src/main/java/com/anabada/etc/BidManagement.java
+++ anabada/src/main/java/com/anabada/etc/BidManagement.java
@@ -49,8 +49,7 @@ public class BidManagement {
             if (minutesPassed >= 10) {
                 SuccessfulBid s = SuccessfulBid.builder().product(product).bid(bid).status(Status.대기).build();
                 successfulBids.add(s);
-                biddingHashMap.remove(product);
-            }
+                mapKey.remove();            }
         }
         return successfulBids;
     }
